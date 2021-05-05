4e762d748a3aaa88a03789a7c795e9f599bdd8c2
Geordie King <geordie.king@gmail.com>
2021-05-05 22:34:36 +0100

    added validator text and some 'plan type todos'

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,11 @@
-
+const validator = require("validator");
+const User = require("../models/User");
 
 // <========== 1) SIGN IN PAGES ==============================
-// User clicks the Sign In button
+// User clicks the Sign In button from the home page
 // If already logged in then goto newsfeed page
 // ELSE goto Sign In page
+// ---------------------------------
 exports.getSignIn = (req, res) => {
   if (req.user) { 
     return res.redirect("/newsfeed.ejs");
@@ -13,13 +15,22 @@ exports.getSignIn = (req, res) => {
   });
 };
 
+// TODO:
+// - postSignIn form - Vaildation
+//      - Validate data
+//        - Inputs (req'd fields)
+//        - Email address exists in DB
+//        - Password matches DB
+//      - ERROR ==> Respond with errors / update UI as needed (reload page??)
+//      - OK ==> Redirect to Newsfeed Page
 // ========== 1) SIGN IN PAGES ==============================>
 
 
 // <========== 2) SIGN UP PAGES ==============================
-// User clicks the Sign Up button
+// User clicks the Sign Up button from the home page
 // IF already logged in then goto newsfeed page
-// ELSE oto Sign Up page
+// ELSE goto Sign Up page
+// ---------------------------------
 exports.getSignUp = (req, res) => {
   if (req.user) { 
     return res.redirect("/newsfeed.ejs");
@@ -28,6 +39,44 @@ exports.getSignUp = (req, res) => {
     title: "Sign Up for an Account",
   });
 };
+ 
+// TODO:
+// - postSignUp form - Vaildation
+//      - Validate data
+//        - Inputs (req'd fields / email format / password length/match)
+//        - Duplicate users
+//      - ERROR ==> Respond with errors / update UI as needed (reload page??)
 
+
+const user = new User({
+  userID: req.body.userName,
+  emailAddr: req.body.email,
+  fullName: req.body.fullName,
+  password: req.body.password,
+});
+
+// TODO:
+// - postSignUp form - Save to DB and then login
+//      - Save data
+//      - OK ==> Redirect to newsfeed page 
 
 // ========== 2) SIGN UP PAGES ==============================>
+
+
+
+// ========== 3) LOGOUT ==============================>
+// User clicks the logout link/button
+// Kill the session
+// Redirect to home page
+// ---------------------------------
+exports.logout = (req, res) => {
+  req.logout();
+  req.session.destroy((err) => {
+    if (err)
+      console.log("Error : Failed to destroy the session during logout.", err);
+    req.user = null;
+    res.redirect("/");
+  });
+};
+
+// ========== 2) LOGOUT ==============================>
---
+++ controllers/validator.js
@@ -0,0 +1,28 @@
+
+
+//TODO:
+// SIGN UP FORM
+// Validate data entry:
+//    - Email address:
+//        - has an @   (MVP)
+//        - maybe try to validate .com/.co.  If we are feeling brave  (STRETCH)
+//    - Password:
+//        - Length = <size>   (MVP)
+//        - Special chars / numbers   (STRETCH)
+//    - Req'd fields
+//        - Username   (MVP)
+//        - Email address   (MVP)
+//        - Password   (MVP)
+//        - Fullname   (MVP)
+// Validate against DB
+//    - Email address ==> Unique in DB   (MVP)
+
+
+// SIGN IN FORM
+// Validate data entry
+//    - Email address:
+//        - has an @   (MVP)
+//        - maybe try to validate .com/.co.  If we are feeling brave   (STRETCH)
+// Validate against DB
+//    - Email address ==> exists in DB   (MVP)
+//    - Password ==> matches the db entry   (MVP)
